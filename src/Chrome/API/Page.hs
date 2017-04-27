f9b1b1a90bf2e8d4b1db46b759790dfa3ba3224b
Thomas Crevoisier <crevoisier.thomas@gmail.com>
2017-04-27 11:58:38 +0200

    Add some Page functions

--- src/Chrome/API/Page.hs
+++ src/Chrome/API/Page.hs
@@ -12,12 +12,16 @@ enable = sendCmd' $ Method "Page.enable" (empty :: Map String String)
 disable :: TargetClient (Maybe Value)
 disable = sendCmd' $ Method "Page.disable" (empty :: Map String String)
 
+-- TODO : add optional parameters "ignoreCache" and "scriptEvaluatedOnLoad"
+reload :: TargetClient (Maybe Value)
+reload = sendCmd' $ Method "Page.reload" (empty :: Map String String)
+
 navigate :: String -> TargetClient (Maybe Value)
 navigate url = sendCmd' $ Method "Page.navigate" (insert "url" url empty)
 
--- TODO : reload
-
--- TODO : handleJavaScriptDialog
+-- TODO : add optional parameter "promptText"
+handleJavaScriptDialog :: Bool -> TargetClient (Maybe Value)
+handleJavaScriptDialog accept = sendCmd' $ Method "Page.handleJavaScriptDialog" (insert "accept" accept empty)
 
 -- TODO : onDomContentEventFired
 
